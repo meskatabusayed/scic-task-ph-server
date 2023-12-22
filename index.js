471a34d9d7a45e462c3cb21cfdda8cd7ed8b566b
meskatabusayed <meskatabusayed@gmail.com>
2023-12-22 15:57:41 +0600

    crud operation created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -10,8 +10,8 @@ app.use(express.json());
 // zPvuywgYyeoVbvQM
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.fohhaen.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = "mongodb+srv://ph-job:zPvuywgYyeoVbvQM@cluster0.fohhaen.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +26,67 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const todoCollection = client.db("todoDB").collection("todo");
+
+    app.post("/api/v1/todo", async (req, res) => {
+      try {
+        const data = req.body;
+        const result = await todoCollection.insertOne(data);
+        res.send({ status: true });
+      } catch (error) {
+        res.send({ status: false });
+      }
+    });
+
+    app.get("/api/v1/todo", async (req, res) => {
+      try {
+        const result = await todoCollection.find().toArray();
+        res.send({ status: true, data: result });
+      } catch (error) {
+        res.send({ status: false, data: [] });
+      }
+    });
+    app.get("/api/v1/todo/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const query = { _id: new ObjectId(id) };
+        const result = await todoCollection.findOne(query);
+        res.send({ status: true, data: result });
+      } catch (error) {
+        res.send({ status: false, data: {} });
+      }
+    });
+
+    app.patch("/api/v1/todo/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const query = { _id: new ObjectId(id) };
+        const data = req.body;
+        const updateDoc = {
+          $set: data,
+        };
+        const result = await todoCollection.updateOne(query, updateDoc);
+        res.send({ status: true });
+      } catch (error) {
+        res.send({ status: false });
+      }
+    });
+    app.delete("/api/v1/todo/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        console.log(id);
+        const result = await todoCollection.deleteOne(query);
+        res.send({ status: true });
+      } catch (error) {
+        res.send({ status: false });
+      }
+    });
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
